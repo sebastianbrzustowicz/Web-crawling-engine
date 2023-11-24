8f721a3f0c2d3b9fa755abd2f126cf139f6ce4a4
sebastianbrzustowicz <Se.Brzustowicz@gmail.com>
2023-11-24 01:55:05 +0100

    normalizeURL and tests

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
+//import { normalizeURL } from './crawl.js';
+
+//normalizeURL("https://stackoverflow.com/questions/950087/how-do-i-include-a-javascript-file-in-another-javascript-file")
+
+
 console.log("hello world")
